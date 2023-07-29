8a6b5f1042c70f1598adab80b2dbfc1563c5cfaf
Rafael Pedro <leega_rafael.pedro@slmandic.edu.br>
2023-07-28 21:05:13 -0300

    first commit

---
+++ includes/class-wghp-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://ween.codes
+ * @since      1.0.0
+ *
+ * @package    Wghp
+ * @subpackage Wghp/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wghp
+ * @subpackage Wghp/includes
+ * @author     Ween Codes <weencodes@gmail.com>
+ */
+class Wghp_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wghp-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://ween.codes
+ * @since      1.0.0
+ *
+ * @package    Wghp
+ * @subpackage Wghp/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wghp
+ * @subpackage Wghp/includes
+ * @author     Ween Codes <weencodes@gmail.com>
+ */
+class Wghp_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
